e85ab4f46f9fb59b20fb706ea9732c8b1c7a2f7a
Viktor Pavlyshyn <lucean7@gmail.com>
2021-01-29 13:27:35 +0200

    add learning_spring

---
+++ src/main/java/com/epam/learningspring/data/repository/GuestRepository.java
@@ -0,0 +1,9 @@
+package com.epam.learningspring.data.repository;
+
+import com.epam.learningspring.data.entity.Guest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long> {
+}
---
+++ src/main/java/com/epam/learningspring/data/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.epam.learningspring.data.repository;
+
+import com.epam.learningspring.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {
+}
